209f48979c4bc3f61a566e268ca445350e5a8cbf
antaww <anta.pizzetta@gmail.com>
2022-06-09 23:45:58 +0200

    background color working for scroll up & down (need better palette)

--- scripts/index.js
+++ scripts/index.js
@@ -9,18 +9,9 @@ function randomColor() {
     if (color1 === color2) {
         return randomColor();
     }
-    console.log(hexToRgb(color1), hexToRgb(color2));
     return [color1, color2];
 }
 
-function hexToRgb(hex) {
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
 
 window.onload = function () {
     let color = 'linear-gradient(to ' + 'bottom' + ', ' + randomColor()[0] + ', ' + randomColor()[1] + ')';
@@ -30,26 +21,30 @@ window.onload = function () {
 
 let myFullpage = new fullpage('#fullpage', {
     autoScrolling: true, keyboardScrolling: true,
-    onLeave: function(origin, destination, direction) {
-        //todo: adapter la couleur en fonction de la direct scroll
+    onLeave: function (origin, destination, direction) {
         let sectionIndex = origin.index;
-        console.log(sectionIndex);
         let section = document.querySelector(`.section:nth-child(${sectionIndex + 1})`);
         let sectionBackground = section.style.background;
-        console.log(sectionBackground);
-        let rgb = sectionBackground.split(' rgb(')[1].split(')')[0].split(',');
+        console.log(sectionIndex + 1, sectionBackground);
+        let rgb;
+        if (direction === 'up') {
+            rgb = sectionBackground.split('rgb(')[1].split(')')[0].split(',');
+        } else if (direction === 'down') {
+            rgb = sectionBackground.split(' rgb(')[1].split(')')[0].split(',');
+        }
         rgb = rgb.map(function (item) {
             return item.trim();
         });
-        console.log(rgb);
-        let color1 = 'rgb(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ')';
-        console.log(color1);
-
-        let color = 'linear-gradient(to ' + 'bottom' + ', ' + color1 + ', ' + randomColor()[0] + ')';
+        let rgbCopiedColor = 'rgb(' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ')';
         let nextSection = destination.item;
+        let color;
+        if (direction === 'down') {
+            color = 'linear-gradient(to ' + 'bottom' + ', ' + rgbCopiedColor + ', ' + randomColor()[0] + ')';
+        } else if (direction === 'up') {
+            color = 'linear-gradient(to ' + 'bottom' + ', ' + randomColor()[0] + ', ' + rgbCopiedColor + ')';
+        }
         nextSection.style.background = color;
     },
-
 })
 
 document.querySelector('.navbar').addEventListener('click', function (e) {
